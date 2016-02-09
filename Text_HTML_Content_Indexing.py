114bae120eae8d9466a32afbaf6b1524c46da4e4
Carlene Liriano <cl2294@gmail.com>
2016-02-09 11:31:27 -0600

    Google Like Content Indexer

--- Text_HTML_Content_Indexing.py
+++ Text_HTML_Content_Indexing.py
@@ -7,8 +7,9 @@ Google Like Content Indexer. Given a directory, this script will index the conte
 """
 
 stop_words = ['a','an','and','i']
-
-#Recursviely walks through a directory to return a list of files in a directory
+'''
+Recursviely walks through a directory to return a list of files in a directory
+'''
 def recursive_find(dirname,file_list):
 	array_all = os.listdir(dirname)
 	for item in array_all:
@@ -21,15 +22,17 @@ def recursive_find(dirname,file_list):
 			else:
 				file_list.append(path)
 	return file_list
-
-#Given a list of files, open each file, and get a list of words from each file
-#Helper methods will verify the file extension and call the appropriate read_file method 
+'''
+Given a list of files, open each file, and get a list of words from each file
+Helper methods will verify the file extension and call the appropriate read_file method 
+'''
 def index_all_files(file_list, dictionary):
     for the_file in file_list:
     	dictionary_builder(check_file_extension(the_file)(read_file(the_file)), the_file, dictionary)
-
-#Given a list of words in a file, build a list of unique words.
-#Each word will be a dictionary key and its value pair will be an array of files containing that word
+'''
+Given a list of words in a file, build a list of unique words.
+Each word will be a dictionary key and its value pair will be an array of files containing that word
+'''
 def dictionary_builder(wordlist, filename, dictionary):
 	for word in wordlist:
 		if word in dictionary.keys():
@@ -43,15 +46,18 @@ def dictionary_builder(wordlist, filename, dictionary):
 			else:
 				continue
 
-
-#Given a file name, open it and spit it out into an array 
+'''
+Given a file name, open it and spit it out into an array 
+'''
 def read_file (filename):
 	tmp_array = []
 	with open(filename, "r") as f:
 		tmp_array = f.readlines()
 	return tmp_array
 
-#Given a filename, check the file extension and return the appropriate function name
+'''
+Given a filename, check the file extension and return the appropriate function name
+'''
 def check_file_extension(filename):
 	index = filename.index('.')
 	extension = filename[index:]
@@ -61,13 +67,14 @@ def check_file_extension(filename):
 		return html_file_to_wordlist
 	else:
 		return unknown
-
 def unknown(filename):
 	print "unknown file type: " + str(filename)
 
-#File_to_wordlist will take a list of lines form a txt file and split them up into an array of words that I can iterate over later
-#Punctuation and spaces will be ignored
-#Returns a list of words
+'''
+File_to_wordlist will take a list of lines form a txt file and split them up into an array of words that I can iterate over later
+Punctuation and spaces will be ignored
+Returns a list of words
+'''
 def file_to_wordlist (array_of_file_lines):
 	word = ""
 	wordlist = []
@@ -86,9 +93,11 @@ def file_to_wordlist (array_of_file_lines):
 					continue
 	return wordlist
 
-#html_tile_to_wordlist will take a list of lines form a .html file and split them up into an array of words that I can iterate over later
-#Punctuation, spaces, and tags will be ignored
-#Returns a list of words
+'''
+html_tile_to_wordlist will take a list of lines form a .html file and split them up into an array of words that I can iterate over later
+Punctuation, spaces, and tags will be ignored
+Returns a list of words
+'''
 def html_file_to_wordlist (array_of_html_lines):
 	word = ""
 	wordlist = []
@@ -108,7 +117,9 @@ def html_file_to_wordlist (array_of_html_lines):
 						continue
 	return wordlist
 
-#helper methods for parsing file text
+'''
+helper methods for parsing file text
+'''
 def is_space(character):
 	return character == ' '
 
@@ -116,8 +127,10 @@ def is_punctuation(character):
 	punctuation = '!"#$%&\n()*+,-./:;<=>?@[\\]^_`{|}~'
 	return character in punctuation
 
-#This will just be a dictionary look up on the keywords
-#Given a dictionary and a list of keywords to search for, this function will return a list of search results for the keywords
+'''
+This will just be a dictionary look up on the keywords
+Given a dictionary and a list of keywords to search for, this function will return a list of search results for the keywords
+'''
 def find_files_with(index,keywords):
 	search_results = {}
 	for word in keywords:
@@ -127,19 +140,23 @@ def find_files_with(index,keywords):
 			search_results[word] = "Not Found"
 	return search_results
 
-#takes a comma delimited string and strips spaces and splits on commas
-#Returns an array with user input
+'''
+Takes a comma delimited string and strips spaces and splits on commas
+Returns an array with user input
+'''
 def user_input_to_array(string):
 	string_list = string.lower().split(',')
 	for index in range(0, len(string_list)):
 		string_list[index] = string_list[index].strip()
 	return string_list
 
-#set up any required data structures and kick off the user input call
-#leverages the PPrint module to pretty print the search results which are a series of dictionary key/value pairs
+'''
+set up any required data structures and kick off the user input call
+leverages the PPrint module to pretty print the search results which are a series of dictionary key/value pairs
+'''
 def index_engine():
 	#hard_coding_directory
-	directory = '/Users/Carly/Documents/GoCode/ContentIndex/Folder3'
+	directory = '/Users/Carly/Documents/GoCode/ContentIndex'
 
 	master_dictionary = {}
 	index_all_files(recursive_find(directory, []), master_dictionary)
